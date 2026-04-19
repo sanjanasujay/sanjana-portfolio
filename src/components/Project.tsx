4a13a60ece275f3378d5d9fb794391dc9cd6c615
sanjanasujay <sanjana121123@gmail.com>
2026-04-19 13:13:11 -0700

    Add latest projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -10,23 +10,42 @@ function Project() {
     <div className="projects-container" id="projects">
         <h1>Personal Projects</h1>
         <div className="projects-grid">
+            
+            <div className="project">
+                <img src={mock05} className="zoom" alt="thumbnail" width={"80%"}/>
+                <a href='https://github.com/sanjanasujay/document-intelligence-chatbot' target='_blank' rel="noreferrer">
+                <h2>Document Intelligence Chatbot using RAG</h2></a>
+                <p>Built an end-to-end Retrieval-Augmented Generation (RAG) pipeline using LangChain and OpenAI sAPI to enable natural language querying over custom PDF documents, reducing hallucinations by
+grounding LLM responses in retrieved document context. Engineered a vector embedding pipeline using ChromaDB to chunk, embed, and index document content, enabling semantic search and accurate context retrieval for user queries with sub-second
+response time.</p>
+            </div>
+             <div className="project">
+                <img src={mock04} className="zoom" alt="thumbnail" width="80%"/>
+                <a href='https://github.com/sanjanasujay/ai-support-triage' target='_blank' rel="noreferrer">
+                <h2>AI-Powered Support Ticket Triage Backend </h2></a>
+                <p>Reduced support triage by 60% by building an LLM-powered backend that classifies tickets, assigns
+urgency, and auto-escalates low-confidence cases, improving routing accuracy and system reliability.</p>
+            </div>
+             <div className="project">
+                <img src={mock04} className="zoom" alt="thumbnail" width="80%"/>
+                <a href='https://github.com/sanjanasujay/distributed-systems-design-playground' target='_blank' rel="noreferrer">
+                <h2>Distributed Systems Design Playground </h2></a>
+                <p>Built an interactive platform for designing distributed system architectures and simulating traffic loads
+and bottlenecks using a Go-based simulation engine.</p>
+            </div>
             <div className="project"  >
                <img src={mock03} className="zoom" alt="thumbnail" width={"80%"}   />
-                {/* <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"></a> */}
-                <h2>Translation of Indic Languages to English and vice-versa using NLP</h2>
+                <a href="https://link.springer.com/chapter/10.1007/978-981-96-0165-3_11" target="_blank" rel="noreferrer">
+                <h2>Translation of Indic Languages to English and vice-versa using NLP</h2></a>
                 <p>Built a transformer-based neural machine translation model to translate multiple Indian languages to English and vice-versa. Integrated text-to-speech functionality to generate audio output of translations, enhancing accessibility for non-English speakers.</p>
+                
             </div>
-            
             <div className="project">
                 <img src={mock05} className="zoom" alt="thumbnail" width={"80%"}/>
                 <h2>Café Database Management System</h2>
                 <p>Developed a full-stack food ordering website using HTML, CSS, JavaScript, PHP, MySQL with dynamic menu-based ordering, enabling real-time order tracking. Through optimized MySQL queries and schema design, ensured smooth transaction handling.</p>
             </div>
-            <div className="project">
-                <img src={mock04} className="zoom" alt="thumbnail" width="80%"/>
-                <h2>Multiple Regression Property Analysis</h2>
-                <p>Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.</p>
-            </div>
+           
              <div className="project">
                 <img src={mock06} className="zoom" alt="thumbnail" width="80%"/>
               <h2>Retail Analytics: A Multi-Model Approach to Demand Forecasting</h2>
