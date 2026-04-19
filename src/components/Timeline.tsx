5a6b7f8d2939096f0acfc05d39116bacbec9821b
sanjanasujay <sanjana121123@gmail.com>
2026-04-19 14:14:26 -0700

    Change role to software instead of content

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -36,7 +36,7 @@ function Timeline() {
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Content Developer - Hewlett Packard Enterprise</h3>
+            <h3 className="vertical-timeline-element-title">Software Developer - Hewlett Packard Enterprise</h3>
             <h4 className="vertical-timeline-element-subtitle">Bengaluru, India</h4>
             <p>Developed web-based training courses on HPE's storage products using React JS, TypeScript, JavaScript.</p>
             <p>Delivered technical training courses at HPE covering Storage Area Network (SAN) technologies, enterprise storage servers, and big data solutions such as HPE Ezmeral Runtime Enterprise, as well as cloud-focused offerings including HPE Private Cloud AI and HPE GreenLake solutions.</p>
@@ -47,7 +47,7 @@ function Timeline() {
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Content Developer Intern - Hewlett Packard Enterprise</h3>
+            <h3 className="vertical-timeline-element-title">Software Developer Intern - Hewlett Packard Enterprise</h3>
             <h4 className="vertical-timeline-element-subtitle">Bengaluru, India</h4>
             <p>
               Developed web-based training courses on HPE's storage products using HTML, CSS, JavaScript.
