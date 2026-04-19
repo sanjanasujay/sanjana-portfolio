84bf110b28d278be71e7e83000c8394f7b3fd76d
sanjanasujay <sanjana121123@gmail.com>
2026-04-19 14:13:40 -0700

    Replace all project images

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,9 @@
+import dicRAG from '../assets/images/dicRAG.png';
+import ais from '../assets/images/aiSupportTriage.png';
+import distpG from '../assets/images/distpG.png';
 import mock03 from '../assets/images/nlp.jpg';
-import mock04 from '../assets/images/mock04.png';
-import mock05 from '../assets/images/mockk.png';
-import mock06 from '../assets/images/ml.png';
+import cafeDB from '../assets/images/cafeDb.png';
+import retail from '../assets/images/retailForecast.png';
 
 import '../assets/styles/Project.scss';
 
@@ -12,22 +14,20 @@ function Project() {
         <div className="projects-grid">
             
             <div className="project">
-                <img src={mock05} className="zoom" alt="thumbnail" width={"80%"}/>
+                <img src={dicRAG} className="zoom" alt="thumbnail" width="80%"/>
                 <a href='https://github.com/sanjanasujay/document-intelligence-chatbot' target='_blank' rel="noreferrer">
                 <h2>Document Intelligence Chatbot using RAG</h2></a>
-                <p>Built an end-to-end Retrieval-Augmented Generation (RAG) pipeline using LangChain and OpenAI sAPI to enable natural language querying over custom PDF documents, reducing hallucinations by
-grounding LLM responses in retrieved document context. Engineered a vector embedding pipeline using ChromaDB to chunk, embed, and index document content, enabling semantic search and accurate context retrieval for user queries with sub-second
-response time.</p>
+                <p>Built an end-to-end RAG pipeline to enable natural language querying over custom PDF documents, reducing hallucinations by grounding LLM responses in retrieved document context. </p>
             </div>
              <div className="project">
-                <img src={mock04} className="zoom" alt="thumbnail" width="80%"/>
+                <img src={ais} className="zoom" alt="thumbnail" width="80%"/>
                 <a href='https://github.com/sanjanasujay/ai-support-triage' target='_blank' rel="noreferrer">
                 <h2>AI-Powered Support Ticket Triage Backend </h2></a>
                 <p>Reduced support triage by 60% by building an LLM-powered backend that classifies tickets, assigns
 urgency, and auto-escalates low-confidence cases, improving routing accuracy and system reliability.</p>
             </div>
              <div className="project">
-                <img src={mock04} className="zoom" alt="thumbnail" width="80%"/>
+                <img src={distpG} className="zoom" alt="thumbnail" width="80%"/>
                 <a href='https://github.com/sanjanasujay/distributed-systems-design-playground' target='_blank' rel="noreferrer">
                 <h2>Distributed Systems Design Playground </h2></a>
                 <p>Built an interactive platform for designing distributed system architectures and simulating traffic loads
@@ -41,16 +41,17 @@ and bottlenecks using a Go-based simulation engine.</p>
                 
             </div>
             <div className="project">
-                <img src={mock05} className="zoom" alt="thumbnail" width={"80%"}/>
+                <img src={retail} className="zoom" alt="thumbnail" width="80%"/>
+              <h2>Retail Analytics: A Multi-Model Approach to Demand Forecasting</h2>
+                <p>Built and compared multiple models (Linear Regression, Decision Tree, Random Forest, XGBoost) to forecast daily product-level revenue 30 days ahead. Engineered time-series and calendar features to capture seasonality and festive effects, and performed scenario-based forecasting to quantify holiday-driven revenue uplift for inventory and promotion planning.</p>
+            </div>
+            <div className="project">
+                <img src={cafeDB} className="zoom" alt="thumbnail" width={"80%"}/>
                 <h2>Café Database Management System</h2>
                 <p>Developed a full-stack food ordering website using HTML, CSS, JavaScript, PHP, MySQL with dynamic menu-based ordering, enabling real-time order tracking. Through optimized MySQL queries and schema design, ensured smooth transaction handling.</p>
             </div>
            
-             <div className="project">
-                <img src={mock06} className="zoom" alt="thumbnail" width="80%"/>
-              <h2>Retail Analytics: A Multi-Model Approach to Demand Forecasting</h2>
-                <p>Built and compared multiple models (Linear Regression, Decision Tree, Random Forest, XGBoost) to forecast daily product-level revenue 30 days ahead. Engineered time-series and calendar features to capture seasonality and festive effects, and performed scenario-based forecasting to quantify holiday-driven revenue uplift for inventory and promotion planning.</p>
-            </div>
+             
            
            
             
